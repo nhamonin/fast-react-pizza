4a30c17677cba62da4185ff71dcf07bec97f7cb1
nhamonin <nazarii.hamonin@gmail.com>
2023-09-17 16:57:02 +0300

    Add createOrder action to CreateOrder component

--- src/services/apiRestaurant.ts
+++ src/services/apiRestaurant.ts
@@ -20,7 +20,7 @@ export async function getOrder(id: string) {
   return data;
 }
 
-export async function createOrder(newOrder: Order) {
+export async function createOrder(newOrder: PartialOrder) {
   try {
     const res = await fetch(`${API_URL}/order`, {
       method: 'POST',
