475c653ac1e99febf2b8f311b7b2dcc1e01fe23f
nhamonin <nazarii.hamonin@gmail.com>
2023-09-17 15:14:13 +0300

    Add AppLayout, Header, and CartOverview components, and update routing

---
+++ src/ui/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza CO.</Link>
+
+      <p>Jonas</p>
+    </header>
+  );
+}
+
+export default Header;
