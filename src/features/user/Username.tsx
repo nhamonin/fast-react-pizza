0785d66d3d6ffdb84ffe4625519b15221d850e29
nhamonin <nazarii.hamonin@gmail.com>
2023-09-19 13:25:30 +0300

    Add redux and react-redux as dependencies, update Username component to use redux store, create userSlice and update main.tsx to use redux store

--- src/features/user/Username.tsx
+++ src/features/user/Username.tsx
@@ -1,5 +1,15 @@
+import { useSelector } from "react-redux";
+
+import { RootState } from "../../store";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const username = useSelector((state: RootState) => state.user.username);
+
+  if (!username) return null;
+
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
 }
 
 export default Username;
