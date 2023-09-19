f7c07c61b10b72f06be21f3c6cd7320a66d2af6e
nhamonin <nazarii.hamonin@gmail.com>
2023-09-19 13:00:21 +0300

    Error component styling

--- src/ui/Error.tsx
+++ src/ui/Error.tsx
@@ -7,10 +7,10 @@ function Error() {
   const error = useRouteError() as ErrorResponse;
 
   return (
-    <div>
-      <h1>Something get wrong :c</h1>
-      <p>{error.data}</p>
-      <p>%MESSAGE%</p>
+    <div className="flex h-screen flex-col items-center justify-center gap-6">
+      <h1 className="text-5xl font-bold">Something went wrong :c</h1>
+      <p className="text-2xl font-semibold">{error.data}</p>
+      <p className="italic">%MESSAGE%</p>
       <button onClick={() => navigate(-1)}></button>
       <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
